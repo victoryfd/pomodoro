55f00bea60de2949f5e6e443b26bb69e52b204d1
victoryfd <victor.yf.dai@gmail.com>
2026-01-09 17:19:50 -0500

    used pathlib for file paths in sound and storage

--- src/sound.py
+++ src/sound.py
@@ -1,4 +1,8 @@
+from pathlib import Path
 import winsound
 
+root = Path(__file__).parent.parent 
+sound_file = root / "assets" / "alert.wav"
+
 def notification_sound():
-    winsound.PlaySound("alert.wav", winsound.SND_FILENAME)
+    winsound.PlaySound(str(sound_file), winsound.SND_FILENAME)
