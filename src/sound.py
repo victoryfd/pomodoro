0e22128b14cf23192330e934133d013139c89d19
Victor Dai <victoryfd@gmail.com>
2026-02-16 11:27:32 -0500

    added linux functionality, will add macOS in the future

--- src/sound.py
+++ src/sound.py
@@ -1,8 +1,23 @@
 from pathlib import Path
-import winsound
+import sys
+import os
+
 
 root = Path(__file__).parent.parent 
 sound_file = root / "assets" / "alert.wav"
 
 def notification_sound():
+    if sys.platform == "win32":
+        _play_windows()
+    else:
+        _play_linux()
+    
+
+def _play_windows():
+    import winsound
     winsound.PlaySound(str(sound_file), winsound.SND_FILENAME)
+
+def _play_linux():
+    os.system(f"paplay {sound_file}")
+
+# def _play_macOS():
